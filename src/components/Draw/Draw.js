551ab3e124cafee0cd2a8c55525bae73c7f57f4c
Bian-Mu <2570772184@qq.com>
2024-10-27 00:14:26 +0800

    feat:Draw

--- src/components/Draw/Draw.js
+++ src/components/Draw/Draw.js
@@ -1,10 +1,52 @@
+import { useEffect, useState } from "react";
+import "./Draw.css"
 
+const topics = ["doggie"]
 
 function Draw() {
+    const [pics, SetPics] = useState([]);
+    useEffect(() => {
+        const newPic = []
+        const loadPic = async () => {
+            for (let topicIndex = 0; topicIndex < topics.length; topicIndex++) {
+                const topic = topics[topicIndex];
+                for (let index = 1; index <= 9; index++) {
+                    const url = `http://localhost:4000/public/2024pic/pic${index}_${topic}.jpg`;
+
+                    try {
+                        const response = await fetch(url);
+                        if (response.status !== 201) {
+                            newPic.push({ src: url, id: `${topicIndex}-${index}` });
+                            SetPics([...newPic]);
+                        }
+                    } catch (error) {
+                        console.log("Error loading")
+                        break;
+                    }
+                }
+            }
+        }
+        loadPic()
+    }, [])
+
     return (
-        <div>
-            this is draw;
-        </div>
+        <>
+            <div id="main-content-draw">
+                <div id="content-draw">
+                    {pics.map(pic => (
+                        <div key={pic.id} className="imgBox">
+                            <img src={pic.src} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <hr />
+            <form action="" method="">
+                <textarea name="comment" id="to-comment" placeholder="对小狗善良一些！"></textarea>
+                <button id='handout' type="submit">Push</button>
+            </form>
+            <div id="logo2">ฅ边.牧ฅノ</div>
+        </>
     )
 }
 
