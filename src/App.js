6f9b38bbb5dbe422dd066620b3eaf72be064762b
Bian-Mu <2570772184@qq.com>
2024-10-31 15:53:12 +0800

    fix:strict

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import { Outlet } from "react-router";
 import Nav from "./components/zother/Nav/Nav"
 import SelfIntro from "./components/zother/SelfIntro/SelfIntro"
 import "./App.css"
+import { StrictMode } from "react";
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <main>
-        <SelfIntro />
-        <article>
-          <Outlet />
-        </article>
+    <StrictMode>
+      <div className="App">
+        <Nav />
+        <main>
+          <SelfIntro />
+          <article>
+            <Outlet />
+          </article>
 
-      </main>
-    </div>
+        </main>
+      </div>
+    </StrictMode>
+
   );
 }
 
