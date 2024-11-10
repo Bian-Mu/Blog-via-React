604df6b4939148eb4eefcf5aae0f1369060f1d0f
Bian-Mu <2570772184@qq.com>
2024-11-10 19:22:48 +0800

    feat:兼容mp3+修复样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Nav from "./components/zother/Nav/Nav"
 import SelfIntro from "./components/zother/SelfIntro/SelfIntro"
 import "./App.css"
 import React from "react";
 function App() {
+  const location = useLocation();
+  const isSong = location.pathname.includes("/song")
   return (
     <div className="App">
       <Nav />
       <main>
         <SelfIntro />
-        <article>
+        <article className={isSong ? 'songLocation' : ""}>
           <Outlet />
         </article>
       </main>
