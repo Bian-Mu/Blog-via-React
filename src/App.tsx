d8a08a44d68ce25db41b69da891f19edba5a98f2
Bian-Mu <2570772184@qq.com>
2024-11-30 18:06:46 +0800

    feat:Novel initial

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import React from "react";
 function App() {
   const location = useLocation();
   const isSong = location.pathname.includes("/song")
+  const isNovel = location.pathname.includes("/novel")
   return (
     <div className="App">
       <Nav />
       <main>
         <SelfIntro />
-        <article className={isSong ? 'songLocation' : ""}>
+        <article className={isSong ? 'songLocation' : (isNovel ? 'novelLocation' : "")}>
           <Outlet />
         </article>
       </main>
--- src/components/Novel/Novel.tsx
+++ src/components/Novel/Novel.tsx
@@ -1,10 +1,35 @@
 import React from "react"
 import "./Novel.css"
+import novelRecord from "../../assets/image/novelRecord.png"
 
 function Novel() {
+
+
     return (
         <div id="main-content-novel">
-            想看的直接私聊主包
+            <div id="novelInfo">
+                <div>
+                    <img id="novelRecord" src={novelRecord} alt="pic" />
+                </div>
+                <div id="novelBrief">
+                    <span>
+                        《将醒》
+                    </span>
+                    <p>
+                        &nbsp;&nbsp;——连载中——
+                    </p>
+                    <span>
+                        剧情介绍:
+                    </span>
+                    <br />
+                    <p>
+                        反复挣扎，笑比哭难看的与几年青春握手言和
+                    </p>
+                </div>
+            </div>
+            <div id="novelLink">
+                暂未发表于互联网，敬请期待
+            </div>
         </div>
     )
 }
